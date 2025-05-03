88bef8f5fd12a15854b990d22f798bb474bf2f6e
MasterLegoYoda <bahrani.ali.2001@gmail.com>
2025-05-03 17:04:20 +0330

    e

--- exercises/03_ticket_v1/05_encapsulation/src/lib.rs
+++ exercises/03_ticket_v1/05_encapsulation/src/lib.rs
@@ -30,8 +30,16 @@ pub mod ticket {
             }
         }
 
-        pub fn title() -> String{
-            return  Ticket.;
+        pub fn title(self) -> String{
+            return  self.title;
+        }
+
+        pub fn description(self) -> String{
+            return  self.description;
+        }
+
+        pub fn status(self) -> String{
+            return self.status
         }
 
         // TODO: Add three public methods to the `Ticket` struct:
